5cd996acb956858a0bdce83b39c753748702e80c
FachryRizano <fachry.rizano@gmail.com>
2022-01-25 00:33:33 +0700

    modify script

---
+++ counter.py
@@ -0,0 +1,75 @@
+from tracker import Tracker
+import imutils
+import cv2
+
+class Counter():
+    def __init__(self):
+        cap, fps, h, w = self.__init_capture()
+        self.vid_writer = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'XVID'), fps, (w, h))
+        self.left_id_arr = []
+        self.right_id_arr = []
+        
+        # Truck, Bus, Car
+        self.object_count ={'Truck':0,'Bus':0,'Car':0}
+        
+        self.tracker = Tracker(model='yolox-s', ckpt='/content/best_ckpt.pth.tar',filter_class=['truck','bus','car'])
+        self.left_right_count = [0,0]
+
+        self.left_border_origin= (0,int(2/3*h))
+        self.left_border_end = (int(w/2)-50,int(2/3*h)+5)
+        self.right_border_origin= (int(w/2)-25,int(2/3*h))
+        self.right_border_end = (w-10,int(2/3*h)+5)
+        self.start_count(cap,h,w)
+
+
+    def __init_capture(self):
+        cap = cv2.VideoCapture('/content/malam_sibuk_20.mp4')  # open one video
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        return cap, fps, h, w
+    
+    def start_count(self,cap,h,w):
+        while True:
+            _, img = cap.read() # read frame from video
+            if img is None:
+                break
+            # im = imutils.resize(img, height=500)
+            image,outputs= self.tracker.update(img)
+            #Left
+            cv2.line(image,(0,int(2/3*h)),(int(w/2)-50,int(2/3*h)),(255,0,0),3)
+            
+            #Right
+            cv2.line(image,(int(w/2),int(2/3*h)),(w-10,int(2/3*h)),(255,0,0),3)
+            # print(outputs)
+            for out in outputs:
+                center_x= out[0] + (out[2] - out[0])/2
+                center_y= out[1] + (out[3] - out[1])/2
+                if out[-2] not in self.left_id_arr and self.left_border_origin[0]<=center_x<=self.left_border_end[1] and self.left_border_origin[1]<=center_y<=self.left_border_end[1]:
+                    self.left_id_arr.append(out[-2])
+                    if int(out[-1]) == 0:
+                        self.object_count['Truck']+=1
+                    elif int(out[-1]) == 1:
+                        self.object_count['Bus']+=1        
+                    elif int(out[-1]) == 2:    
+                        self.object_count['Car']+=1
+                    self.left_right_count[0]+=1
+                if out[-2] not in self.right_id_arr and self.right_border_origin[0]<=center_x<=self.right_border_end[0] and self.right_border_origin[1]<=center_y<=self.right_border_end[1]:
+                    self.right_id_arr.append(out[-2])
+                    if int(out[-1]) == 0:
+                        self.object_count['Truck']+=1
+                    elif int(out[-1]) == 1:
+                        self.object_count['Bus']+=1        
+                    elif int(out[-1]) == 2:    
+                        self.object_count['Car']+=1
+                    self.left_right_count[1]+=1
+                # im = imutils.resize(image, height=h,width=w)
+                # image = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+                # vid_writer.write(image) 
+        cap.release()
+
+if __name__ == "__main__":
+    counter = Counter()
+    print('Object Count:',counter.object_count)
+    print('Left Right Count:', counter.left_right_count)
+
--- deep_sort/deep_sort/sort/tracker.py
+++ deep_sort/deep_sort/sort/tracker.py
@@ -1,138 +1,42 @@
-# vim: expandtab:ts=4:sw=4
-from __future__ import absolute_import
-import numpy as np
-from . import kalman_filter
-from . import linear_assignment
-from . import iou_matching
-from .track import Track
+import sys
+sys.path.insert(0, './YOLOX')
+from YOLOX.yolox.data.datasets.coco_classes import COCO_CLASSES
+from detector import Detector
+from deep_sort.utils.parser import get_config
+from deep_sort.deep_sort import DeepSort
+import torch
+import cv2
+from utils.visualize import vis_track
 
 
-class Tracker:
-    """
-    This is the multi-target tracker.
+class_names = COCO_CLASSES
 
-    Parameters
-    ----------
-    metric : nn_matching.NearestNeighborDistanceMetric
-        A distance metric for measurement-to-track association.
-    max_age : int
-        Maximum number of missed misses before a track is deleted.
-    n_init : int
-        Number of consecutive detections before the track is confirmed. The
-        track state is set to `Deleted` if a miss occurs within the first
-        `n_init` frames.
+class Tracker():
+    def __init__(self, filter_class=None, model='yolox-s', ckpt='yolox_s.pth.tar', ):
+        self.detector = Detector(model, ckpt)
+        cfg = get_config()
+        cfg.merge_from_file("deep_sort/configs/deep_sort.yaml")
+        self.deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
+                            max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
+                            nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                            max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
+                            use_cuda=True)
+        self.filter_class = filter_class
 
-    Attributes
-    ----------
-    metric : nn_matching.NearestNeighborDistanceMetric
-        The distance metric used for measurement to track association.
-    max_age : int
-        Maximum number of missed misses before a track is deleted.
-    n_init : int
-        Number of frames that a track remains in initialization phase.
-    kf : kalman_filter.KalmanFilter
-        A Kalman filter to filter target trajectories in image space.
-    tracks : List[Track]
-        The list of active tracks at the current time step.
+    def update(self, image):
+        info = self.detector.detect(image, visual=False)
+        outputs = []
+        if info['box_nums']>0:
+            bbox_xywh = []
+            scores = []
+            #bbox_xywh = torch.zeros((info['box_nums'], 4))
+            for (x1, y1, x2, y2), class_id, score  in zip(info['boxes'],info['class_ids'],info['scores']):
+                if self.filter_class and class_names[int(class_id)] not in self.filter_class:
+                    continue
+                bbox_xywh.append([int((x1+x2)/2), int((y1+y2)/2), x2-x1, y2-y1])
+                scores.append(score)
+            bbox_xywh = torch.Tensor(bbox_xywh)
+            outputs = self.deepsort.update(bbox_xywh, scores, image,info['class_ids'])
+            image = vis_track(image, outputs)
 
-    """
-
-    def __init__(self, metric, max_iou_distance=0.7, max_age=70, n_init=3):
-        self.metric = metric
-        self.max_iou_distance = max_iou_distance
-        self.max_age = max_age
-        self.n_init = n_init
-
-        self.kf = kalman_filter.KalmanFilter()
-        self.tracks = []
-        self._next_id = 1
-
-    def predict(self):
-        """Propagate track state distributions one time step forward.
-
-        This function should be called once every time step, before `update`.
-        """
-        for track in self.tracks:
-            track.predict(self.kf)
-
-    def update(self, detections):
-        """Perform measurement update and track management.
-
-        Parameters
-        ----------
-        detections : List[deep_sort.detection.Detection]
-            A list of detections at the current time step.
-
-        """
-        # Run matching cascade.
-        matches, unmatched_tracks, unmatched_detections = \
-            self._match(detections)
-
-        # Update track set.
-        for track_idx, detection_idx in matches:
-            self.tracks[track_idx].update(
-                self.kf, detections[detection_idx])
-        for track_idx in unmatched_tracks:
-            self.tracks[track_idx].mark_missed()
-        for detection_idx in unmatched_detections:
-            self._initiate_track(detections[detection_idx])
-        self.tracks = [t for t in self.tracks if not t.is_deleted()]
-
-        # Update distance metric.
-        active_targets = [t.track_id for t in self.tracks if t.is_confirmed()]
-        features, targets = [], []
-        for track in self.tracks:
-            if not track.is_confirmed():
-                continue
-            features += track.features
-            targets += [track.track_id for _ in track.features]
-            track.features = []
-        self.metric.partial_fit(
-            np.asarray(features), np.asarray(targets), active_targets)
-
-    def _match(self, detections):
-
-        def gated_metric(tracks, dets, track_indices, detection_indices):
-            features = np.array([dets[i].feature for i in detection_indices])
-            targets = np.array([tracks[i].track_id for i in track_indices])
-            cost_matrix = self.metric.distance(features, targets)
-            cost_matrix = linear_assignment.gate_cost_matrix(
-                self.kf, cost_matrix, tracks, dets, track_indices,
-                detection_indices)
-
-            return cost_matrix
-
-        # Split track set into confirmed and unconfirmed tracks.
-        confirmed_tracks = [
-            i for i, t in enumerate(self.tracks) if t.is_confirmed()]
-        unconfirmed_tracks = [
-            i for i, t in enumerate(self.tracks) if not t.is_confirmed()]
-
-        # Associate confirmed tracks using appearance features.
-        matches_a, unmatched_tracks_a, unmatched_detections = \
-            linear_assignment.matching_cascade(
-                gated_metric, self.metric.matching_threshold, self.max_age,
-                self.tracks, detections, confirmed_tracks)
-
-        # Associate remaining tracks together with unconfirmed tracks using IOU.
-        iou_track_candidates = unconfirmed_tracks + [
-            k for k in unmatched_tracks_a if
-            self.tracks[k].time_since_update == 1]
-        unmatched_tracks_a = [
-            k for k in unmatched_tracks_a if
-            self.tracks[k].time_since_update != 1]
-        matches_b, unmatched_tracks_b, unmatched_detections = \
-            linear_assignment.min_cost_matching(
-                iou_matching.iou_cost, self.max_iou_distance, self.tracks,
-                detections, iou_track_candidates, unmatched_detections)
-
-        matches = matches_a + matches_b
-        unmatched_tracks = list(set(unmatched_tracks_a + unmatched_tracks_b))
-        return matches, unmatched_tracks, unmatched_detections
-
-    def _initiate_track(self, detection):
-        mean, covariance = self.kf.initiate(detection.to_xyah())
-        self.tracks.append(Track(
-            mean, covariance, self._next_id, self.n_init, self.max_age,
-            detection.feature))
-        self._next_id += 1
+        return image, outputs
